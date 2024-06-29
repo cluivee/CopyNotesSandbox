255a29056a92805d46b4f87b1dd9bae7e009616c
Clive <cluivee@gmail.com>
2024-06-29 11:44:49 +0100

    Initial Commit

---
+++ CopyNotesSandbox/CopyNotesSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyNotesSandboxApp.swift
+//  CopyNotesSandbox
+//
+//  Created by Clive on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CopyNotesSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
