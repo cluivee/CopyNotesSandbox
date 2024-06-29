44613784d5170690e31ef248bc3492217b186d71
Clive <cluivee@gmail.com>
2024-06-29 12:22:36 +0100

    Initial Commit with Sidebar layout

---
+++ CopyNotesSandbox/DetailView.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  SwiftUI_Mac_BigSur_Clive_Tutorial
+//
+//  Created by Clive on 19/06/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let httpStatus: HttpStatus
+    @State private var catImage: NSImage?
+
+    var body: some View {
+         VStack {
+            Text("HTTP Status Code: \(httpStatus.code)")
+                .font(.headline)
+                .padding()
+            Text(httpStatus.title)
+                .font(.title)
+            
+            if catImage != nil {
+                Image(nsImage: catImage!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            self.getCatImage()
+        }
+    }
+    
+    func getCatImage() {
+        let url = httpStatus.imageUrl
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                DispatchQueue.main.async {
+                    self.catImage = NSImage(data: data)
+                }
+            }
+        }
+        task.resume()
+    }
+
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(httpStatus: HttpStatus(code: "404", title: "Not Found"))
+    }
+}
---
+++ CopyNotesSandbox/HttpStatus.swift
@@ -0,0 +1,37 @@
+//
+//  HttpStatus.swift
+//  CopyNotesSandbox
+//
+//  Created by Clive on 29/06/2024.
+//
+
+import Foundation
+
+struct HttpStatus: Identifiable, Decodable {
+    let id = UUID()
+    let code: String
+    let title: String
+    
+    var imageUrl: URL {
+        let address = "https://http.cat/\(code).jpg"
+        return URL(string: address)!
+    }
+
+//    enum CodingKeys: String, CodingKey {
+//        case code
+//        case title
+//    }
+}
+
+struct HttpSection: Identifiable, Decodable {
+    let id = UUID()
+    let headerCode: String
+    let headerText: String
+    let statuses: [HttpStatus]
+
+//    enum CodingKeys: String, CodingKey {
+//        case headerCode
+//        case headerText
+//        case statuses
+//    }
+}
